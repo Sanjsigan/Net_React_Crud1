76dafbcb127ca9cda72110aab3268c8c24dd3e12
Sanjsigan <sanjsi0507@gmail.com>
2020-09-30 09:59:07 +0530

    Rect + .Net Crud 2 start to modify

---
+++ Net_React_Crud1/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Net_React_Crud1
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
